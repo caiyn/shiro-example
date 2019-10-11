280afbad3ace8b933dfdcda3c5fe2635ab506884
caiyn <caiyaning@elensdata.com>
2019-10-11 17:19:54 +0800

    test

--- shiro-example/src/main/java/com/example/shiroexample/controller/LoginController.java
+++ shiro-example/src/main/java/com/example/shiroexample/controller/LoginController.java
@@ -1,4 +1,17 @@
 package com.example.shiroexample.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/shiro")
 public class LoginController {
+
+    @GetMapping("/success")
+    public String success(){
+        return "success";
+    }
 }
